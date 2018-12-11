7e27d28b40c4c9af4bac652d1c09e967afea1f50
TimofeyTTT <45222181+TimofeyTTT@users.noreply.github.com>
2018-12-11 09:51:03 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-  $("button.btn").click(function(){
-    var inot=parseInt($('input[name=method]:checked').val());
-    var prom=parseInt($('input[name=diff]:checked').val());
-    var learn=parseInt($('input[name=type]:checked').val());
-    var pro=parseInt($('input[name=op]:checked').val());
-    var wed=parseInt($('input[name=array]:checked').val());
+  $("#submitAnswer").click(function(){
 
-    var tiGet=$("input[name=method]:checked").value;
-    var wiGet=$("input[name=diff]:checked").value;
-    var triGet=$("input[name=type]:checked").value;
-    var youGet=$("input[name=op]:checked").value;
-    var weGet=$("input[name=array]:checked").value;
+    var radio=[];
+    radio.push($("input:radio[name=bum]:checked").val());
+    radio.push($("input:radio[name=diff]:checked").val());
+    radio.push($("input:radio[name=type]:checked").val());
+    radio.push($("input:radio[name=op]:checked").val());
+    radio.push($("input:radio[name=array]:checked").val());
 
-    var answer= tiGet + wiGet + triGet + youGet + weGet;
-    if(tiGet<1 || wiGet<1 || triGet<1 || youGet<1 || weGet<1){
-        $('.output').html("final");
-    }else if(final>0){
-      $('.output').html("<b>Your Score: " +final+ "%</b>");
-    }else {
-      $('.output').html("<b>Failed</b>");
+    var result = 0;
+    if(radio[0] == "10"){result+=10;
     }
-
-  $(".well").show(final);
+    if(radio[1] == "10"){result+=10;
+    }
+    if(radio[2] == "10"){result+=10;
+    }
+    if(radio[3] == "10"){result+=10;
+    }
+    if(radio[4] == "10"){result+=10;
+    }
+    $("#number-correct").val(result);
+    $(".col-md-10").hide();
+    $("#finalOutput").show();
   });
 });
